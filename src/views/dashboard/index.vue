a86a91353425981b5c8f38964cad3d168179b179
xiyuanyuan <xiyuanyan@meituan.com>
2022-10-07 17:07:38 +0800

    trest

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <div style="height: 50vh; width: 50%">
+  <div style="height: 100vh; width: 80%">
     <!-- {{ pageInfo }} -->
     {{ page }} {{ total }}
     <!-- @pages-rendered="afterCreated"  -->
-    <VuePdfApp page-scale="page-fit" pdf="compressed.tracemonkey-pldi-09.pdf" :config="config" @open="openHandler" @pages-rendered="onRender" />
+    <VuePdfApp page-scale="page-fit" :pdf="pdf" :config="config" @open="openHandler" @pages-rendered="onRender" />
   </div>
 </template>
 
 <script>
 import VuePdfApp from 'vue-pdf-app'
-
+import 'vue-pdf-app/dist/icons/main.css'
 export default {
   components: {
     VuePdfApp
@@ -18,20 +18,48 @@ export default {
     return {
       page: 1,
       total: 0,
+      pdf: null,
       // config 用于控制pdf外观
       config: {
-        secondaryToolbar: false,
+        // secondaryToolbar: true,
+        secondaryToolbar: {
+          secondaryPresentationMode: false,
+          secondaryOpenFile: false,
+          secondaryPrint: false,
+          secondaryDownload: false,
+          secondaryViewBookmark: true,
+          firstPage: true,
+          lastPage: true,
+          pageRotateCw: true,
+          pageRotateCcw: true,
+          cursorSelectTool: true,
+          cursorHandTool: true,
+          scrollVertical: true,
+          scrollHorizontal: true,
+          scrollWrapped: true,
+          spreadNone: true,
+          spreadOdd: true,
+          spreadEven: true,
+          documentProperties: true
+        },
         toolbar: {
           toolbarViewerLeft: {
-            findbar: false,
-            previous: false,
-            next: false,
+            findbar: true,
+            previous: true,
+            next: true,
             pageNumber: true
           },
-          toolbarViewerRight: false,
+          // toolbarViewerRight: true,
+          toolbarViewerRight: {
+            presentationMode: true,
+            openFile: false,
+            print: false,
+            download: false,
+            viewBookmark: true
+          },
           toolbarViewerMiddle: {
-            zoomOut: false,
-            zoomIn: false,
+            zoomOut: true,
+            zoomIn: true,
             scaleSelectContainer: true
           }
         }
@@ -39,17 +67,27 @@ export default {
     }
   },
   computed: {
-
+  },
+  mounted() {
+    this.fetch()
   },
   methods: {
+    async fetch() {
+      const result = await fetch('https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf')
+      const blob = await result.blob()
+      this.pdf = await blob.arrayBuffer()
+    },
     onRender(pdfApp) {
       pdfApp.pdfViewer.eventBus.on('pagechanging', ({ pageNumber }) => {
         console.log('11111111111,', pageNumber)
         // TODO： 页码变化的事件
-        this.page = pageNumber
+        if (this.page < this.total) {
+          this.page = pageNumber
+        }
       })
     },
     openHandler(pdfApp) {
+      console.log(pdfApp, '--------')
       this.total = pdfApp.pagesCount
     }
   }
@@ -57,5 +95,8 @@ export default {
 </script>
 
 <style>
-@import "https://unpkg.com/vue-pdf-app@2.0.0/dist/icons/main.css";
+/* @import "https://unpkg.com/vue-pdf-app@2.0.0/dist/icons/main.css"; */
+/* @import "https://unpkg.com/vue-pdf-app@2.0.0/dist/icons/main.css"; */
+/* @import "vue" */
+* { touch-action: pan-y; }
 </style>
